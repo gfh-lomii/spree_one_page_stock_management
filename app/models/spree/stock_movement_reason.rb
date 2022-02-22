f87d8f54ba1c72548ae2b1a85b34e19e18ee25b1
rmartinezapp <robert.martinez.app@gmail.com>
2022-02-22 06:23:50 -0300

    add has_many inventories ti stock_movement_reason

--- app/models/spree/stock_movement_reason.rb
+++ app/models/spree/stock_movement_reason.rb
@@ -3,6 +3,8 @@ module Spree
     acts_as_paranoid
 
     has_many :stock_movements, class_name: 'Spree::StockMovement', foreign_key: :reason_id
+    has_many :inventories, class_name: 'Spree::Inventory'
+
     validates :reason, presence: true
 
     scope :only_enable, -> { where("enabled = true") }
